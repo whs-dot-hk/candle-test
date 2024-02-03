cc2d1eed57decde349bf7407e35ebeb0a4fd60ff
whs <hswongac@gmail.com>
2024-02-03 09:06:40 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
+  inputs.crane.url = "github:ipetkov/crane";
+  inputs.fenix.url = "github:nix-community/fenix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = {
-    self,
+    crane,
+    fenix,
     flake-utils,
     nixpkgs,
+    self,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
@@ -13,14 +17,34 @@
         inherit system;
         config.allowUnfree = true;
       };
+
+      toolchain = with fenix.packages.${system};
+        combine [
+          default.cargo
+          default.rustc
+        ];
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+      src = pkgs.lib.cleanSourceWith {
+        src = ./.;
+        filter = _: _: true;
+      };
     in
       with pkgs; {
-        devShell = with pkgs;
-          mkShell.override {stdenv = gcc11Stdenv;} {
-            nativeBuildInputs = [cudatoolkit];
-            CUDA_COMPUTE_CAP = 86;
-            CUDA_PATH = cudatoolkit;
-            LD_LIBRARY_PATH = "/run/opengl-driver/lib";
-          };
+        devShell = mkShell.override {stdenv = gcc11Stdenv;} {
+          nativeBuildInputs = [cudatoolkit];
+          CUDA_COMPUTE_CAP = 86;
+          CUDA_PATH = cudatoolkit;
+          LD_LIBRARY_PATH = "/run/opengl-driver/lib";
+        };
+        packages.default = craneLib.buildPackage {
+          stdenv = gcc11Stdenv;
+          inherit src;
+          nativeBuildInputs = [cudaPackages.autoAddOpenGLRunpathHook];
+          buildInputs = [cudatoolkit];
+          CUDA_COMPUTE_CAP = 86;
+          CUDA_PATH = cudatoolkit;
+        };
       });
 }
